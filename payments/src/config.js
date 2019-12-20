d0ea6ebd35ae016d5bf3d2c352e25f6e65e411a3
calumbird <42486146+calumbird@users.noreply.github.com>
2019-12-19 16:31:11 -0800

    Payments development (#5)
    * Updated global README
    
    * Updated payments API README
    
    * Added a router and a test route to the payments API

---
+++ payments/src/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const Config = {
+    AccessControl(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header(
+            'Access-Control-Allow-Methods',
+            'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+        );
+        res.header(
+            'Access-Control-Allow-Headers',
+            'X-Requested-With, content-type, x-access-token'
+        );
+        res.header('Access-Control-Allow-Credentials', true);
+        if ('OPTIONS' === req.method) {
+            res.sendStatus(200);
+        } else {
+            console.log(`${req.ip} ${req.method} ${req.url}`);
+            next();
+        }
+    }
+};
+
+module.exports = Config;
