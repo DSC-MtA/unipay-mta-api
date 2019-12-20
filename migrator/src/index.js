e085992b9363062bfb5becba4e279cee1fac9375
Eliot Scott <eliotvscott@gmail.com>
2019-12-20 02:31:13 -0400

    Create migrator service

--- migrator/src/index.js
+++ migrator/src/index.js
@@ -1,3 +1,12 @@
-const {createDb, migrate} = require('postgres-migrations');
+const DBMigrate = require('db-migrate');
+const dbm = DBMigrate.getInstance(true);
 
-console.log('Running migrations');
+function migrateUp(){
+    console.log('running migrations [up]...');
+    dbm.reset()
+        .then(() => dbm.up());
+    console.log('successfully ran migrations [up]...');
+}
+
+// Wait for database to start up, then run migrations
+setTimeout(migrateUp, 1000);
--- migrator/src/migrations/00001_create-initial-tables.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS Users (
-    name varchar (255) UNIQUE,
-    PRIMARY KEY (name)
-);
